65ea525b3b20f353b663d1e3b2311eab56b5a9fa
rawanfatima <rawanfatima@outlook.com>
2020-07-26 22:06:41 -0700

    Updates

---
+++ precache-manifest.6cb84bcd0f45600c0ce2099bc97c9319.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b78518e4bf9dc95f6ea8cbc88961ef2",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "5ffcf45adb17d0090f4b",
+    "url": "/port/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a2542c6b27ac25b9eed3",
+    "url": "/port/static/css/main.d0b994b8.chunk.css"
+  },
+  {
+    "revision": "5ffcf45adb17d0090f4b",
+    "url": "/port/static/js/2.6f964048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/port/static/js/2.6f964048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2542c6b27ac25b9eed3",
+    "url": "/port/static/js/main.861b5059.chunk.js"
+  },
+  {
+    "revision": "8377aaca52fa0eda1adb",
+    "url": "/port/static/js/runtime-main.5676016a.js"
+  },
+  {
+    "revision": "90f0ec03dfd66bb7af37da588585e0c2",
+    "url": "/port/static/media/hero.90f0ec03.jpg"
+  }
+]);
